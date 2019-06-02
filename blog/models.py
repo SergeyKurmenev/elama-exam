fa1d119d47d4c4850011c75202e2bc9e580e2946
Sergey <sergeykurmenev@gmail.com>
2019-06-02 14:34:32 +0300

    ISSUE-10 Создание модели БД
    В файл `models.py` добавлено описание таблицы `Comments`.

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,14 @@ class Post(db.Model):
     is_draft = db.Column(db.Boolean, nullable=False)
     tag = db.Column(db.String(Config.TAG_MAX_LENGTH), nullable=True)
 
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.string(Config.COMMENT_TITLE_MAX_LENGTH), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
