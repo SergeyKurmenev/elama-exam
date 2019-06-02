9bbeebdcaaf0fffda103967d34e954f2ba0ede85
Sergey <sergeykurmenev@gmail.com>
2019-06-02 16:07:08 +0300

    ISSUE-25 Добавление конфигурации для БД в файл config.py

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     POST_TITLE_MAX_LENGTH = 100
 
