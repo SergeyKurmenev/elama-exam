93b68591761d5985f5c3625bb6c3042bd877e084
Sergey <sergeykurmenev@gmail.com>
2019-06-02 14:26:02 +0300

    ISSUE-10 Создание модели БД
    Добавлен файл `models.py`. В него добавлено описание
    таблицы `Posts`.

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from blog import db
+
+from config import Config
+
+
+class Post(db.Model):
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(Config.POST_TITLE_MAX_LENGTH), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    is_draft = db.Column(db.Boolean, nullable=False)
+    tag = db.Column(db.String(Config.TAG_MAX_LENGTH), nullable=True)
+
