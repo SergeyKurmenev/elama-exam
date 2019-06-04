4f607edd667133d3c810daaa403cd2a59d1e200d
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-06-04 15:33:25 +0300

    ISSUE-102 Рефакторинг server_api.py
    В `blog/__init__.py` добавлен импорт класса `Comments`
    из `blog/resource/comments.py`.

--- blog/__init__.py
+++ blog/__init__.py
@@ -19,4 +19,4 @@ from blog import models
 from blog import server_api
 from blog.resources.posts import Posts
 from blog.resources.statistic import Statistic
-
+from blog.resources.comments import Comments
