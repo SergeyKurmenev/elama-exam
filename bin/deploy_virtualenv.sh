a378e65ac20151f65a449f7e4b92e1bd731f7bec
Sergey <sergeykurmenev@gmail.com>
2019-06-04 19:55:23 +0300

    ISSUE-116 Обновлён скрипт deploy_virtualenv.sh
    В лог файл дополнительно добавляется
    содержимое `requirements.txt`

--- bin/deploy_virtualenv.sh
+++ bin/deploy_virtualenv.sh
@@ -9,7 +9,15 @@ source ./venv/bin/activate
 pip install -r requirements.txt
 log_file="./logs/virtualenv/venv.`date '+%Y-%m-%d_%H-%M'`.log"
 
+#Эта часть отвечает за создание лога
 python --version >> ${log_file}
+echo >> ${log_file}
+echo '====================' >> ${log_file}
+echo 'requirements.txt:' >> ${log_file}
+echo >> ${log_file}
+cat requirements.txt >> ${log_file}
+echo '====================' >> ${log_file}
+echo 'pip freeze:' >> ${log_file}
 echo >> ${log_file}
 pip freeze >> ${log_file}
 
