9f834670836b34940451a3a9b399865cb2ec9c83
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-06-05 12:49:58 +0300

    ISSUE-118 Вынесение создания лога virtualenv в отдельный скрипт
    Часть отвечающая за создания лога virtualenv в `deploy_virtualenv.sh`
    заменена на вызов скрипта.

--- bin/deploy_virtualenv.sh
+++ bin/deploy_virtualenv.sh
@@ -9,15 +9,5 @@ source ./venv/bin/activate
 pip install -r requirements.txt
 
 #Эта часть отвечает за создание лога
-log_file="./logs/virtualenv/venv.`date '+%Y-%m-%d_%H-%M'`.log"
-python --version >> ${log_file}
-echo >> ${log_file}
-echo '====================' >> ${log_file}
-echo 'requirements.txt:' >> ${log_file}
-echo >> ${log_file}
-cat requirements.txt >> ${log_file}
-echo '====================' >> ${log_file}
-echo 'pip freeze:' >> ${log_file}
-echo >> ${log_file}
-pip freeze >> ${log_file}
+source ./bin/make_venv_log.sh
 
