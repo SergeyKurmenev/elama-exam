ccdb0d71ada4b5f42359c54d96dd66f59d084d27
Sergey <sergeykurmenev@gmail.com>
2019-06-09 13:39:44 +0300

    ISSUE-197 Добавлена CLI команда для получения всех комментариев поста

--- cli/comment.py
+++ cli/comment.py
@@ -31,7 +31,24 @@ def make_comment(post_id, email, name, body):
         print(response.json(), f"Status code: {response.status_code}")
 
 
+@click.command(help='Получение всех комментариев поста')
+@click.option('--post-id', '-i', help='Id поста комментарии которого необходимо получить')
+def get_comments(post_id):
+    data = {}
+
+    if post_id:
+        data['post_id'] = post_id
+
+    response = requests.get('http://127.0.0.1:5000/api/v1/comments', data=data)
+    if response.status_code == 200:
+        print(response.status_code)
+    else:
+        print(response.json(), f"Status code: {response.status_code}")
+
+
 comment.add_command(make_comment)
+comment.add_command(get_comments)
+
 
 if __name__ == '__main__':
     comment()
