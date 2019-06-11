eedfbbbeebfe693d5718adef161f3f87e7f21c2f
Sergey <sergeykurmenev@gmail.com>
2019-06-11 17:02:56 +0300

    ISSUE-246 Добавлено создание тестовых категорий в БД при сборке

--- fill_db_with_demo_data.py
+++ fill_db_with_demo_data.py
@@ -6,8 +6,15 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from blog import db
 
+from blog.models import Category
 from blog.models import Post
 from blog.models import Comment
+
+categories_count = 5
+
+for number in range(categories_count):
+    category_for_add = Category(f'Category number {number + 1}', f'#tag{number + 1}')
+    db.session.add(category_for_add)
 
 posts_url = "https://jsonplaceholder.typicode.com/posts"
 comments_url = "https://jsonplaceholder.typicode.com/comments"
@@ -22,7 +29,6 @@ for post in posts:
 
     db.session.add(post_for_add)
 
-
 for comment in comments:
     comment_for_add = Comment(post_id=comment['postId'],
                               email=comment['email'],
@@ -36,4 +42,3 @@ try:
 except SQLAlchemyError as e:
     print('Не удалось записать занные в БД.'
           f'Причина: {str(e)}')
-
