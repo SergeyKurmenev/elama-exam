625ea59f8137c74dfbf8f1d8dbf9bf2882353d12
Sergey <sergeykurmenev@gmail.com>
2019-06-13 20:44:47 +0300

    ISSUE-367 Добавлено форматирование выводимых JSON в change_category

--- cli/category.py
+++ cli/category.py
@@ -62,7 +62,9 @@ def change_category(category_id, name, tag):
         click.echo(click.style(f"Status code: {response.status_code}", fg='green'))
     else:
         click.echo(click.style(f"Status code: {response.status_code}", fg='red'))
-        click.echo(response.json())
+        click.echo(json.dumps(obj=response.json(),
+                              indent=2,
+                              sort_keys=True))
 
 
 @click.command(help='Метод удаления категории')
