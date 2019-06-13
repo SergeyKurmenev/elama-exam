ce161c4a84d551ac5f58fd8e3d40663d7207d913
Sergey <sergeykurmenev@gmail.com>
2019-06-13 20:46:43 +0300

    ISSUE-367 Добавлено форматирование выводимых JSON в change_tag

--- cli/post.py
+++ cli/post.py
@@ -57,7 +57,9 @@ def change_tag(post_id, tag):
         click.echo(click.style(f"Status code: {response.status_code}", fg='green'))
     else:
         click.echo(click.style(f"Status code: {response.status_code}", fg='red'))
-        click.echo(response.json())
+        click.echo(json.dumps(obj=response.json(),
+                              indent=2,
+                              sort_keys=True))
 
 
 @click.command(help='Метод удаления постов')
